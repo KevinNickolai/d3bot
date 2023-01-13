19d300523ed733227c69c06bb03c58f7f6d00e94
KevinNickolai <nickolaikevin@gmail.com>
2023-01-12 22:44:12 -0500

    Added -u flag for user mentions in d3top command.

--- dist/commands/d3top.js
+++ dist/commands/d3top.js
@@ -23,67 +23,132 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const Discord = __importStar(require("discord.js"));
 const TempleOSRS_1 = __importStar(require("../classes/TempleOSRS"));
 const config_1 = require("../config");
 module.exports = {
     name: 'd3top',
     aliases: ['d3t'],
     description: "See top District 3 players over the past Month, Week, or Day",
-    usage: `${config_1.prefix}d3top <day | week | month>`,
+    usage: `${config_1.prefix}d3top <(d)ay | (w)eek | (m)onth> <-u>`,
     args: true,
     execute(message, args) {
         let tOsrs = new TempleOSRS_1.default();
-        let timeframe = args.shift()?.toLowerCase();
+        const timeframe = args.shift()?.toLowerCase();
+        const mentionsOn = args.shift()?.toLowerCase() === "-u";
         switch (timeframe) {
             case "day":
             case "d":
                 tOsrs.QueryCurrentTop(TempleOSRS_1.TempleCompetitiveCommunityEndpointEnum.CurrentTopDay, config_1.DISTRICT3_TEMPLE_GROUPID)
-                    .then((resultingJSON) => {
+                    .then(async (resultingJSON) => {
                     let result = resultingJSON;
-                    let formattedResponse = "```";
-                    for (var i = 1; typeof result.Day[i] !== 'undefined' && i <= 25; ++i) {
-                        formattedResponse += `${result.Day[i].rank.toString().padStart(2)} (${result.Day[i].xp.slice(0, 5)}): ${result.Day[i].nickname.padEnd(12)}\t`;
-                        if (i % 5 === 0) {
+                    let discordUsersFound = [];
+                    for (let i = 1; typeof result.Day[i] !== 'undefined' && i <= 10; ++i) {
+                        let cachedPlayer = message.guild?.members.cache.find(user => user.nickname === result.Day[i].player);
+                        if (cachedPlayer) {
+                            discordUsersFound.push(Promise.resolve(new Discord.Collection((new Map()).set(cachedPlayer.nickname, cachedPlayer))));
+                        }
+                        else {
+                            discordUsersFound.push(message.guild?.members.search({ query: result.Day[i].player }));
+                        }
+                    }
+                    if (mentionsOn)
+                        await Promise.all(discordUsersFound);
+                    let formattedResponse = mentionsOn ? "" : "```";
+                    for (let i = 1; typeof result.Day[i] !== 'undefined' && ((!mentionsOn && i <= 25) || (mentionsOn && i <= 10)); ++i) {
+                        let player;
+                        const padLimit = mentionsOn ? 13 : 12;
+                        if (mentionsOn)
+                            player = (await discordUsersFound[i - 1])?.find(user => user.nickname === result.Day[i].player);
+                        formattedResponse += `${result.Day[i].rank.toString().padStart(2)} ` +
+                            `(${result.Day[i].xp.slice(0, 5)}): ` +
+                            `${player ? `<@${player.id}>` : result.Day[i].player.padEnd(padLimit)}\t`;
+                        if (mentionsOn || i % 5 === 0) {
                             formattedResponse += "\n";
                         }
                     }
                     formattedResponse.trimEnd();
-                    formattedResponse += "```";
-                    message.reply(`Top D3 ${result.info.duration} ${result.info.skill}:\n` + formattedResponse);
+                    if (!mentionsOn)
+                        formattedResponse += "```";
+                    message.reply({ "content": `Top D3 ${result.info.duration} ${result.info.skill}:\n` + formattedResponse, "allowedMentions": {
+                            "parse": []
+                        } });
                 });
                 break;
             case "week":
             case "w":
                 tOsrs.QueryCurrentTop(TempleOSRS_1.TempleCompetitiveCommunityEndpointEnum.CurrentTopWeek, config_1.DISTRICT3_TEMPLE_GROUPID)
-                    .then((resultingJSON) => {
+                    .then(async (resultingJSON) => {
                     let result = resultingJSON;
-                    let formattedResponse = "```";
-                    for (var i = 1; typeof result.Week[i] !== 'undefined' && i <= 25; ++i) {
-                        formattedResponse += `${result.Week[i].rank.toString().padStart(2)} (${result.Week[i].xp.slice(0, 5)}): ${result.Week[i].nickname.padEnd(12)}\t`;
-                        if (i % 5 === 0) {
+                    let discordUsersFound = [];
+                    for (let i = 1; typeof result.Week[i] !== 'undefined' && i <= 10; ++i) {
+                        let cachedPlayer = message.guild?.members.cache.find(user => user.nickname === result.Week[i].player);
+                        if (cachedPlayer) {
+                            discordUsersFound.push(Promise.resolve(new Discord.Collection((new Map()).set(cachedPlayer.nickname, cachedPlayer))));
+                        }
+                        else {
+                            discordUsersFound.push(message.guild?.members.search({ query: result.Week[i].player }));
+                        }
+                    }
+                    if (mentionsOn)
+                        await Promise.all(discordUsersFound);
+                    let formattedResponse = mentionsOn ? "" : "```";
+                    for (let i = 1; typeof result.Week[i] !== 'undefined' && ((!mentionsOn && i <= 25) || (mentionsOn && i <= 10)); ++i) {
+                        let player;
+                        const padLimit = mentionsOn ? 13 : 12;
+                        if (mentionsOn)
+                            player = (await discordUsersFound[i - 1])?.find(user => user.nickname === result.Week[i].player);
+                        formattedResponse += `${result.Week[i].rank.toString().padStart(2)} ` +
+                            `(${result.Week[i].xp.slice(0, 5)}): ` +
+                            `${player ? `<@${player.id}>` : result.Week[i].player.padEnd(padLimit)}\t`;
+                        if (mentionsOn || i % 5 === 0) {
                             formattedResponse += "\n";
                         }
                     }
                     formattedResponse.trimEnd();
-                    formattedResponse += "```";
-                    message.reply(`Top D3 ${result.info.duration} ${result.info.skill}:\n` + formattedResponse);
+                    if (!mentionsOn)
+                        formattedResponse += "```";
+                    message.reply({ "content": `Top D3 ${result.info.duration} ${result.info.skill}:\n` + formattedResponse, "allowedMentions": {
+                            "parse": []
+                        } });
                 });
                 break;
             case "month":
             case "m":
                 tOsrs.QueryCurrentTop(TempleOSRS_1.TempleCompetitiveCommunityEndpointEnum.CurrentTopMonth, config_1.DISTRICT3_TEMPLE_GROUPID)
-                    .then((resultingJSON) => {
+                    .then(async (resultingJSON) => {
                     let result = resultingJSON;
-                    let formattedResponse = "```";
-                    for (var i = 1; typeof result.Month[i] !== 'undefined' && i <= 25; ++i) {
-                        formattedResponse += `${result.Month[i].rank.toString().padStart(2)} (${result.Month[i].xp.slice(0, 5)}): ${result.Month[i].nickname.padEnd(12)}\t`;
-                        if (i % 5 === 0) {
+                    let discordUsersFound = [];
+                    for (let i = 1; typeof result.Month[i] !== 'undefined' && i <= 10; ++i) {
+                        let cachedPlayer = message.guild?.members.cache.find(user => user.nickname === result.Month[i].player);
+                        if (cachedPlayer) {
+                            discordUsersFound.push(Promise.resolve(new Discord.Collection((new Map()).set(cachedPlayer.nickname, cachedPlayer))));
+                        }
+                        else {
+                            discordUsersFound.push(message.guild?.members.search({ query: result.Month[i].player }));
+                        }
+                    }
+                    if (mentionsOn)
+                        await Promise.all(discordUsersFound);
+                    let formattedResponse = mentionsOn ? "" : "```";
+                    for (let i = 1; typeof result.Month[i] !== 'undefined' && ((!mentionsOn && i <= 25) || (mentionsOn && i <= 10)); ++i) {
+                        let player;
+                        const padLimit = mentionsOn ? 13 : 12;
+                        if (mentionsOn)
+                            player = (await discordUsersFound[i - 1])?.find(user => user.nickname === result.Month[i].player);
+                        formattedResponse += `${result.Month[i].rank.toString().padStart(2)} ` +
+                            `(${result.Month[i].xp.slice(0, 5)}): ` +
+                            `${player ? `<@${player.id}>` : result.Month[i].player.padEnd(padLimit)}\t`;
+                        if (mentionsOn || i % 5 === 0) {
                             formattedResponse += "\n";
                         }
                     }
                     formattedResponse.trimEnd();
-                    formattedResponse += "```";
-                    message.reply(`Top D3 ${result.info.duration} ${result.info.skill}:\n` + formattedResponse);
+                    if (!mentionsOn)
+                        formattedResponse += "```";
+                    message.reply({ "content": `Top D3 ${result.info.duration} ${result.info.skill}:\n` + formattedResponse, "allowedMentions": {
+                            "parse": []
+                        } });
                 });
                 break;
             default:
--- src/commands/d3top.ts
+++ src/commands/d3top.ts
@@ -10,27 +10,51 @@ module.exports = {
     name: 'd3top',
     aliases: ['d3t'],
     description: "See top District 3 players over the past Month, Week, or Day",
-    usage: `${prefix}d3top <day | week | month>`,
+    usage: `${prefix}d3top <(d)ay | (w)eek | (m)onth> <-u>`,
     args: true,
     execute(message: Discord.Message, args: string[]){
 
         let tOsrs = new TempleOSRS();
 
-        let timeframe = args.shift()?.toLowerCase();
+        const timeframe = args.shift()?.toLowerCase();
+
+        const mentionsOn = args.shift()?.toLowerCase() === "-u";
 
         switch (timeframe){
             case "day":
             case "d":
                 tOsrs.QueryCurrentTop(TempleCompetitiveCommunityEndpointEnum.CurrentTopDay, DISTRICT3_TEMPLE_GROUPID)
-                    .then((resultingJSON) => {
+                    .then(async (resultingJSON) => {
                         let result = resultingJSON as IBaseTopTimeframe;
-                        
-                        let formattedResponse = "```"
-                        for(var i = 1; typeof result.Day[i] !== 'undefined' && i <= 25; ++i){
 
-                            formattedResponse += `${result.Day[i].rank.toString().padStart(2)} (${result.Day[i].xp.slice(0, 5)}): ${result.Day[i].nickname.padEnd(12)}\t`;
+                        let discordUsersFound = [];
 
-                            if(i % 5 === 0){
+                        for(let i = 1; typeof result.Day[i] !== 'undefined' && i <= 10; ++i){
+                            let cachedPlayer = message.guild?.members.cache.find(user => user.nickname === result.Day[i].player);
+
+                            if(cachedPlayer){
+                                discordUsersFound.push(Promise.resolve(new Discord.Collection((new Map<string, Discord.GuildMember>()).set(cachedPlayer.nickname!, cachedPlayer))));
+                            }
+                            else{
+                                discordUsersFound.push(message.guild?.members.search({query: result.Day[i].player}));
+                            }
+                        }
+
+                        if(mentionsOn) await Promise.all(discordUsersFound);
+
+                        let formattedResponse =  mentionsOn ?  "" : "```"
+                        for(let i = 1; typeof result.Day[i] !== 'undefined' && ((!mentionsOn && i <= 25) || (mentionsOn && i<=10)); ++i){
+
+                            let player;
+
+                            const padLimit = mentionsOn ? 13 : 12;
+
+                            if(mentionsOn) player = (await discordUsersFound[i-1])?.find(user => user.nickname === result.Day[i].player);
+                            formattedResponse += `${result.Day[i].rank.toString().padStart(2)} ` +
+                                                 `(${result.Day[i].xp.slice(0, 5)}): ` +
+                                                 `${ player ? `<@${player.id}>` : result.Day[i].player.padEnd(padLimit)}\t`;
+
+                            if(mentionsOn || i % 5 === 0){
                                 formattedResponse+="\n";
                             }
 
@@ -38,25 +62,47 @@ module.exports = {
 
                         formattedResponse.trimEnd();
 
-                        formattedResponse += "```";
+                        if (!mentionsOn) formattedResponse += "```";
 
-                        message.reply(`Top D3 ${result.info.duration} ${result.info.skill}:\n` + formattedResponse);
+                        message.reply({"content": `Top D3 ${result.info.duration} ${result.info.skill}:\n` + formattedResponse, "allowedMentions": {
+                            "parse": []
+                        }});
                     });
-                
-                
                 break;
             case "week":
             case "w":
                 tOsrs.QueryCurrentTop(TempleCompetitiveCommunityEndpointEnum.CurrentTopWeek, DISTRICT3_TEMPLE_GROUPID)
-                .then((resultingJSON) => {
+                .then(async (resultingJSON) => {
                     let result = resultingJSON as IBaseTopTimeframe;
-                    
-                    let formattedResponse = "```"
-                    for(var i = 1; typeof result.Week[i] !== 'undefined' && i <= 25; ++i){
 
-                        formattedResponse += `${result.Week[i].rank.toString().padStart(2)} (${result.Week[i].xp.slice(0, 5)}): ${result.Week[i].nickname.padEnd(12)}\t`;
+                    let discordUsersFound = [];
 
-                        if(i % 5 === 0){
+                    for(let i = 1; typeof result.Week[i] !== 'undefined' && i <= 10; ++i){
+                        let cachedPlayer = message.guild?.members.cache.find(user => user.nickname === result.Week[i].player);
+
+                        if(cachedPlayer){
+                            discordUsersFound.push(Promise.resolve(new Discord.Collection((new Map<string, Discord.GuildMember>()).set(cachedPlayer.nickname!, cachedPlayer))));
+                        }
+                        else{
+                            discordUsersFound.push(message.guild?.members.search({query: result.Week[i].player}));
+                        }
+                    }
+
+                    if(mentionsOn) await Promise.all(discordUsersFound);
+
+                    let formattedResponse =  mentionsOn ?  "" : "```"
+                    for(let i = 1; typeof result.Week[i] !== 'undefined' && ((!mentionsOn && i <= 25) || (mentionsOn && i<=10)); ++i){
+
+                        let player;
+
+                        const padLimit = mentionsOn ? 13 : 12;
+
+                        if(mentionsOn) player = (await discordUsersFound[i-1])?.find(user => user.nickname === result.Week[i].player);
+                        formattedResponse += `${result.Week[i].rank.toString().padStart(2)} ` +
+                                             `(${result.Week[i].xp.slice(0, 5)}): ` +
+                                             `${ player ? `<@${player.id}>` : result.Week[i].player.padEnd(padLimit)}\t`;
+
+                        if(mentionsOn || i % 5 === 0){
                             formattedResponse+="\n";
                         }
 
@@ -64,24 +110,48 @@ module.exports = {
 
                     formattedResponse.trimEnd();
 
-                    formattedResponse += "```";
+                    if (!mentionsOn) formattedResponse += "```";
 
-                    message.reply(`Top D3 ${result.info.duration} ${result.info.skill}:\n` + formattedResponse);
+                    message.reply({"content": `Top D3 ${result.info.duration} ${result.info.skill}:\n` + formattedResponse, "allowedMentions": {
+                        "parse": []
+                    }});
                 });
                 
                 break;
             case "month":
             case "m":
                 tOsrs.QueryCurrentTop(TempleCompetitiveCommunityEndpointEnum.CurrentTopMonth, DISTRICT3_TEMPLE_GROUPID)
-                .then((resultingJSON) => {
+                .then(async (resultingJSON) => {
                     let result = resultingJSON as IBaseTopTimeframe;
-                    
-                    let formattedResponse = "```"
-                    for(var i = 1; typeof result.Month[i] !== 'undefined' && i <= 25; ++i){
 
-                        formattedResponse += `${result.Month[i].rank.toString().padStart(2)} (${result.Month[i].xp.slice(0, 5)}): ${result.Month[i].nickname.padEnd(12)}\t`;
+                    let discordUsersFound = [];
 
-                        if(i % 5 === 0){
+                    for(let i = 1; typeof result.Month[i] !== 'undefined' && i <= 10; ++i){
+                        let cachedPlayer = message.guild?.members.cache.find(user => user.nickname === result.Month[i].player);
+
+                        if(cachedPlayer){
+                            discordUsersFound.push(Promise.resolve(new Discord.Collection((new Map<string, Discord.GuildMember>()).set(cachedPlayer.nickname!, cachedPlayer))));
+                        }
+                        else{
+                            discordUsersFound.push(message.guild?.members.search({query: result.Month[i].player}));
+                        }
+                    }
+
+                    if(mentionsOn) await Promise.all(discordUsersFound);
+
+                    let formattedResponse =  mentionsOn ?  "" : "```"
+                    for(let i = 1; typeof result.Month[i] !== 'undefined' && ((!mentionsOn && i <= 25) || (mentionsOn && i<=10)); ++i){
+
+                        let player;
+
+                        const padLimit = mentionsOn ? 13 : 12;
+
+                        if(mentionsOn) player = (await discordUsersFound[i-1])?.find(user => user.nickname === result.Month[i].player);
+                        formattedResponse += `${result.Month[i].rank.toString().padStart(2)} ` +
+                                             `(${result.Month[i].xp.slice(0, 5)}): ` +
+                                             `${ player ? `<@${player.id}>` : result.Month[i].player.padEnd(padLimit)}\t`;
+
+                        if(mentionsOn || i % 5 === 0){
                             formattedResponse+="\n";
                         }
 
@@ -89,9 +159,11 @@ module.exports = {
 
                     formattedResponse.trimEnd();
 
-                    formattedResponse += "```";
+                    if (!mentionsOn) formattedResponse += "```";
 
-                    message.reply(`Top D3 ${result.info.duration} ${result.info.skill}:\n` + formattedResponse);
+                    message.reply({"content": `Top D3 ${result.info.duration} ${result.info.skill}:\n` + formattedResponse, "allowedMentions": {
+                        "parse": []
+                    }});
                 });
                 break;
             default:
